57dc0abdaaff265f4848c3706d743755321d97f9
ItsDimitrie <laue0002@HZ.nl>
2023-11-28 09:05:15 +0100

    Hotfix backend / frontend

--- dynamischeGateway/routes/index.js
+++ dynamischeGateway/routes/index.js
@@ -4,40 +4,23 @@ const axios = require("axios");
 const registry = require("./registry.json");
 
 router.all("/:apiName/:path/*", (req, res) => {
-  // Check if the requested API is registered
+  console.log("this works");
   if (registry.services[req.params.apiName]) {
-    // Perform a request to the request api
     axios({
       method: req.method,
-      url: registry.services[req.params.apiName].url + req.params.path + '/' + req.params['0'],
-      headers: req.headers,
+      url: registry.services[req.params.apiName].url + req.params.path + '/' + req.params['0'],    headers: {
+        ...req.headers,
+        'Cache-Control': 'no-cache',  // Add this header to disable caching
+      },
       data: req.body,
-    })
-    
-    // If succesful, get the response data
-    .then((response) => {
+    }).then((response) => {
       res.send(response.data);
-    })
-    
-    // Handle potential errors
-    .catch((reason) => {
-      // Check if the error has to do with a 304 within the axios service
-      if (reason.message.includes('304')) {
-        // Redirect the user to the same url to retry
-        res.redirect(req.url);
-        return;
-      }
-
-      // There is another problem, return the issue
-      res.status(500);
-      res.send(reason);
+    }).catch((err) => {
+      console.error(err);
     });
-  }
-  
-  // The API is not registered, tell the user
-  else {
+  } else {
     res.status(404);
-    res.send("API does not exist in registry.");
+    res.send("Api name does not exist in registry");
   }
 });
 
