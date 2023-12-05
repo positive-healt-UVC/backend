3b7c831e198a3101f4951798408085ad79b4c02c
Ali Hamdan <112816648+Ali-A-Hamdan@users.noreply.github.com>
2023-12-05 11:01:59 +0100

    add jest test to test if the event table to exist

---
+++ events/tests/database.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const { connectDB, initializeDB } = require('../database/database');
+initializeDB();
+
+describe('Database Tests', function () {
+
+    it('Database should have the "events" table', function (done) {
+        const db = connectDB();
+
+        // Query to check if the "events" table exists
+        db.all("PRAGMA table_info(events)", function (error, columns) {
+            if (error) {
+                done(error);
+            } else {
+                // Check if there are columns, indicating that the table exists
+                assert.ok(columns && columns.length > 0, 'Expected "events" table to exist and has ', columns.length, ' columns');
+                done();
+            }
+
+            db.close();
+            });
+    });
+
+});
