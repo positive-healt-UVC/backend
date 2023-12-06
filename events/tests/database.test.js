fc6d951f25af95c90fc5262e68a16bf34f293d70
Ali Hamdan <112816648+Ali-A-Hamdan@users.noreply.github.com>
2023-12-06 09:25:11 +0100

    adjust database test

--- events/tests/database.test.js
+++ events/tests/database.test.js
@@ -1,24 +1,10 @@
-const assert = require('assert');
-const { connectDB, initializeDB } = require('../database/database');
+const { initializeDB, getAllEvents } = require('../database/database');
 
 describe('Database Tests', function () {
-
-    it('Database should have the "events" table', function (done) {
+    test('Connect', () => {
         initializeDB();
-        const db = connectDB();
+        const events = getAllEvents();
 
-        // Query to check if the "events" table exists
-        db.all("PRAGMA table_info(events)", function (error, columns) {
-            if (error) {
-                done(error);
-            } else {
-                // Check if there are columns, indicating that the table exists
-                assert.ok(columns && columns.length > 0, 'Expected "events" table to exist and has ', columns.length, ' columns');
-                done();
-            }
-
-            db.close();
-            });
-    });
-
+        expect(typeof events).toBe('object');
+    })
 });
