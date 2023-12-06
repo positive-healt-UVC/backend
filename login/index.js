e4788d1c91caee7d7a093ff921a26346bfed6707
Wesley van Schaijk <scha0092@hz.nl>
2023-12-06 12:30:33 +0100

    Fixed Connection Issues

--- login/index.js
+++ login/index.js
@@ -1,89 +1,7 @@
 // Import necessary modules
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
-
-// Import database-related logic
-const sqlite3 = require('sqlite3').verbose();
-
-// Connect to the database
-function connectDB() {
-  try {
-    const db = new sqlite3.Database('./database/users.db'); // Change to "users.db"
-    return db;
-  } catch (error) {
-    console.error('Error opening database:', error);
-  }
-}
-
-// Initialize the database
-function initializeDB() {
-  const db = connectDB();
-
-  // Setup error handling
-  db.on("error", function(error) {
-    console.log("Error initializing table: ", error);
-  }); 
-
-  // Run the command to create the "users" table if not exists
-  db.run(`CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    password TEXT NOT NULL,
-    age INTEGER NOT NULL,
-    handicap TEXT NOT NULL
-  )`);
-
-  // Close the database connection
-  console.log('Database initialized.');
-  db.close();
-}
-
-// Get all users from the database
-function getAllUsers() {
-  const db = connectDB();
-
-  // Setup error handling
-  db.on("error", function(error) {
-    console.log("Error reading users: ", error);
-  }); 
-
-  // Get all rows from the "users" table and return them
-  return new Promise((resolve, reject) => {
-    db.all('SELECT * FROM users', (error, rows) => { // Change to "users"
-      console.log(rows);
-      resolve(rows);
-    });
-  });
-}
-
-// Populate the database with dummy data
-function populateDB() {
-  const db = connectDB();
-  // Your logic for populating the database
-}
-
-// Insert a user into the "users" table
-function insertUser(user) {
-  const db = connectDB();
-  
-  // Insert user data into the "users" table
-  db.serialize(() => {
-    const insertStmt = db.prepare(
-      'INSERT INTO users (name, password, age, handicap) VALUES (?, ?, ?, ?)'
-    );
-
-    // Insert user data into the database
-    insertStmt.run(user.name, user.password, user.age, user.handicap);
-
-    // Finalize the insertion
-    insertStmt.finalize();
-    console.log('User data inserted into the "users" table.');
-  });
-
-  // Close the database connection
-  db.close();
-}
+const database  = require('./database/database');
 
 // Initialize the application
 const app = express();
@@ -91,9 +9,6 @@ const app = express();
 // Setup CORS and JSON parsing middleware
 app.use(cors());
 app.use(express.json());
-
-// Initialize the database on startup
-initializeDB();
 
 // Save dummy data to the server
 app.post('/populate-database', async (req, res) => {
@@ -109,7 +24,7 @@ app.post('/populate-database', async (req, res) => {
 // Get all user data from the "users" table
 app.get('/users', cors(), async (req, res, next) => { // Change to "/users"
   try {
-    const users = await getAllUsers();
+    const users = await database.getAllUsers();
     res.json(users);
   } catch (error) {
     console.error('Error fetching user data:', error);
@@ -146,20 +61,9 @@ app.post('/users', cors(), async (req, res) => { // Change to "/users"
 
 // Start the server
 const server = app.listen(process.env.PORT || 3011, () => {
-  // Send a registration request to another service (if needed)
-  axios({
-    method: 'POST',
-    url: 'http://login:3011/users', // Change to "/users"
-    headers: { 'Content-Type': 'application/json' },
-    data: {
-      apiName: "users",
-      protocol: "http",
-      host: 'login',
-      port: 3011,
-    }
-  }).then((response) => {
-    console.log(response.data);
-  });
-
+  // Tell the user about the state of the server 
   console.log(`🍿 Express running → PORT ${server.address().port}`);
+  
+  // Initialize the database on startup
+  database.initializeDB();
 });
